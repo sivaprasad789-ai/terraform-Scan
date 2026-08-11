c7cd750fef0f472ccf8c7094fede23ae80502c93
sivaprasad789-ai <46105970+sivaprasad789-ai@users.noreply.github.com>
2026-08-11 12:03:58 +0400

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
-  # bucket does not have versioning
+  # bucket does not have versioning  
   bucket        = "${local.resource_prefix.value}-data"
   force_destroy = true
   tags = merge({
